1bbbd30746d866b13ed37fe46f95537a9924d2b8
Hung Nguyen <hungnguyen.k51.ftu@gmail.com>
2024-02-21 20:53:16 +0700

    Updated Login in UserController

--- server/src/main/java/maxnguyen/server/controller/UserController.java
+++ server/src/main/java/maxnguyen/server/controller/UserController.java
@@ -8,12 +8,10 @@ import maxnguyen.server.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/users")
 public class UserController {
     private final UserService userService;
